3925cf7bd3c0e52ae164c013c67a9780c0cdff76
Harsh Kashyap <122665995+Imharsh08@users.noreply.github.com>
2026-03-23 11:09:55 +0530

    Create RepositoryShowcase.tsx component

---
+++ src/components/RepositoryShowcase.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RepositoryShowcase: React.FC = () => {
+    return (
+        <div>
+            <h1>Repository Showcase</h1>
+            {/* Add your showcase content here */}
+        </div>
+    );
+};
+
+export default RepositoryShowcase;
