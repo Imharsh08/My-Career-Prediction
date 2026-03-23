0281ef812e5cf0bae8f06848449804d67ecfc0fe
Harsh Kashyap <122665995+Imharsh08@users.noreply.github.com>
2026-03-23 12:31:15 +0530

    Refactor vite.config.ts by removing unused imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Career-Prediction/', // Set this to your repository name
+  base: '/My-Career-Prediction/',
 })
